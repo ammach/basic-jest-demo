88b052a228383fb2946bcf0b77523dd4b17e060d
Ammach Mohamed Amine <ammach@users.noreply.github.com>
2019-06-07 20:15:00 +0000

    chore: Normalize squeleton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-function sum(a, b) {
-  return a + b;
-}
-
-module.exports = {
-  sum
-};
+module.exports = {};
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,3 @@
-const { sum } = require("./index");
-
-describe("tests of index file", () => {
-  it("sould add numbers", () => {
-    expect(sum(2, 3)).toEqual(5);
-  });
+describe("index file", () => {
+  it("", () => {});
 });
